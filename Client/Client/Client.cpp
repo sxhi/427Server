b81b9a13896173d3579ffa6c937dee7d721ded39
Ray Sahi <sxhi@umich.edu>
2022-02-05 20:30:52 -0500

    Add project files.

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "WS2_32.lib")
+#define SERVER_PORT 54000
+
+using namespace std;
+
+void main() {
+
+	string ipAddress = "127.0.0.1"; // Ip address of server
+	int port = SERVER_PORT;
+
+	// Initialize winsock
+	WSADATA data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0) {
+		cerr << "Cant start winsock! Error #" << wsResult << endl;
+		return;
+	}
+
+	// Create Socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) {
+		cerr << "Cannot create socket!" << endl;
+		WSACleanup();
+		return;
+	}
+
+	// Fill in hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(SERVER_PORT);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	int connection = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connection == SOCKET_ERROR) {
+		cerr << "Cant connect to server!" << endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+
+	// Do While loop to send and recieve data
+	char buf[4096];
+	string userInput;
+
+	do {
+		// Prompt user for text
+		cout << "> ";
+		getline(cin, userInput);
+
+		if (userInput.size() > 0) {  // Make sure user typed something in
+			// Send text
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR) {
+
+				// Wait for response
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived > 0) {
+					// Echo response to console
+					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+				}
+			}
+		}
+
+	} while (userInput.size() > 0);
+
+	// Close down client
+	closesocket(sock);
+	WSACleanup();
+}
---
+++ Server/Server/Server.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#pragma comment (lib, "ws2_32.lib")
+#define SERVER_PORT 54000
+using namespace std;
+
+void main()
+{
+	// Initilize winsock
+	WSADATA WsData;
+	WORD ver = MAKEWORD(1, 0);
+	int wsOk = WSAStartup(ver, &WsData);
+
+	if (wsOk != 0) {
+		cerr << "Cant initialize winsock!" << endl;
+		return;
+	}
+
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET) {
+		cerr << "Cant create a socket!" << endl;
+		return;
+	}
+	// Bind socket to ip and port
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(SERVER_PORT);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	// Tell winsock the socket is for listening
+	listen(listening, SOMAXCONN);
+
+	// Wait for a connection
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	if (clientSocket == INVALID_SOCKET) {
+		cerr << "Cannot bind client socket!" << endl;
+		return;
+	}
+
+	char host[NI_MAXHOST]; // Clients remote name
+	char service[NI_MAXHOST]; // Service (i.e. port) the client is connect on
+
+	ZeroMemory(host, NI_MAXHOST); // For Mac
+	ZeroMemory(service, NI_MAXHOST); // For Mac
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+		cout << host << " successfully connected on port " << service << endl;
+	}
+	else {
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << "connected on port" << ntohs(client.sin_port) << endl;
+	}
+
+	// Close listening socket
+	closesocket(listening);
+
+	// While loop: accept and echo message back to client
+	char buf[4096];
+
+	while (true) {
+		ZeroMemory(buf, 4096);
+
+		// Wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR) {
+			cerr << "Error in recv()" << endl;
+			break;
+		}
+		if (bytesReceived == 0) {
+			cout << "Client Disconnected" << endl;
+			break;
+		}
+		cout << string(buf, 0, bytesReceived) << endl;
+		send(clientSocket, buf, bytesReceived + 1, 0);
+	}
+	// Close the socket
+	closesocket(clientSocket);
+
+	// Shutdown winsock
+	WSACleanup();
+}
